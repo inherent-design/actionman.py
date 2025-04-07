e64c8f58584800332aa2df50c0b6dba2fba2e65b
mannie-exe <manpreet.mannie.singh@gmail.com>
2025-04-06 21:19:09 -0700

    Bump version to 0.3.1

--- actionman/__init__.py
+++ actionman/__init__.py
@@ -4,4 +4,4 @@ This package provides a command-line interface for managing C++ projects
 built with CMake, including configuration, building, testing, and running.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
